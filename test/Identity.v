68e3a357efbafb8a6b6fe47de91b8d5d5d448461
Ali Assaf <ali.assaf@inria.fr>
2013-07-16 14:32:47 +0200

    Add multiple identity functions to test polymorphism.

--- test/Identity.v
+++ test/Identity.v
@@ -1,3 +1,10 @@
-Definition id :=
+Definition id1 :=
   fun (A : Type) (x : A) => x.
 
+Definition id2 :=
+  fun (A : Type) (x : A) => x.
+
+Definition id3 :=
+  fun (A : Type) (x : A) => x.
+
+Definition id4 := id1 (forall (A : Type), A -> A) id2.
