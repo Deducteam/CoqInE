b6b033bd0f252879b980ab7a6a4601e9ec416a77
Ali Assaf <ali.assaf@inria.fr>
2013-07-18 13:38:18 +0200

    Properly translate and escape names in the Dedukti namespace.

--- src/name.ml
+++ src/name.ml
@@ -2,6 +2,17 @@
 
 (* The name "names.ml" is already taken by a file in the Coq library. *)
 
+(** There are many different types of names in Coq:
+    - identifier: basic identifier (i.e. string)
+    - name: name used by binders (i.e. "fun x => ...")
+    - dir_path: directory paths (e.g. "A.B.C")
+    - label: names of structure elements
+    - mod_bound_id: names of functor parameters
+    - module_path: module paths (e.g. "<file>.A.B", "<bound_mod_id>.A.B")
+    - constant: refers to a declared constant
+    - inductive: refers to an inductive type
+    - constructor: refers to a constructor of an inductive type *)
+
 let is_alpha c =
   match c with
   | 'a' .. 'z'
@@ -40,9 +51,28 @@ let translate_name name =
   | Names.Name(identifier) -> Names.string_of_id identifier
   | Names.Anonymous -> ""
 
+let translate_dir_path dir_path =
+  escape (Names.string_of_dirpath dir_path)
+
 let translate_label label =
-  Names.string_of_label label
+  escape (Names.string_of_label label)
+
+let translate_mod_bound_id mod_bound_id =
+  escape (Names.string_of_mbid mod_bound_id)
+
+(** Translate the path corresponding to [module_path] followed by [labels]. *)
+let translate_module_path module_path labels =
+  let rec split module_path labels =
+    match module_path with
+    | Names.MPfile(dir_path) -> (translate_dir_path dir_path, labels)
+    | Names.MPbound(mod_bound_id) -> (translate_mod_bound_id mod_bound_id, labels)
+    | Names.MPdot(module_path, label) -> split module_path (label :: labels) in
+  let (prefix, labels) = split module_path labels in
+  let ids = List.map Names.id_of_label labels in
+  (* Reverse the order of ids as dirpath elements are given in reverse order. *)
+  let suffix = translate_dir_path (Names.make_dirpath (List.rev ids)) in
+  Printf.sprintf "%s.%s" (escape prefix) (escape suffix)
 
 let translate_constant constant =
-  Names.string_of_con constant
+  translate_module_path (Names.con_modpath constant) [Names.con_label constant]
 
