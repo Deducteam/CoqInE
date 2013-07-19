9ce982df6f1113f30581d18e400fa4270c9e302f
Ali Assaf <ali.assaf@inria.fr>
2013-07-19 13:58:51 +0200

    Switch from generating a list of dedukti instructions to printing the instructions directly.
    This will make the translation of terms that need global declarations
    such as lets and fixpoints easier.

--- src/export.ml4
+++ src/export.ml4
@@ -4,10 +4,9 @@ let export filename =
   Library.require_library_from_file None filename None;
   let dir_path = Libnames.dirpath_of_string filename in
   let full_filename = Library.library_full_filename dir_path in
-  let translation = Libraries.translate_library dir_path in
   let out = open_out (Filename.chop_extension full_filename ^ ".dk") in
-  List.iter (Dedukti.print out) translation;
-  close_out out
+  try Libraries.translate_library out dir_path
+  with e -> (close_out out; raise e)
 
 VERNAC COMMAND EXTEND Dedukti
 | [ "Dedukti" "Export" string_list(filenames) ] -> [ List.iter export filenames ]
--- src/modules.ml
+++ src/modules.ml
@@ -9,35 +9,35 @@ let translate_constant_type env constant_type =
   | PolymorphicArity(rel_context, polymorphic_arity) ->
       failwith "Polymorphic arity"
 
-let translate_constant_body env label constant_body =
+let translate_constant_body out env label constant_body =
   let name = Name.translate_label label in
   (* TODO: Handle [constant_body.const_hyps] *)
   let const_type' = translate_constant_type env constant_body.const_type in
   match constant_body.const_body with
   | Undef(inline) ->
-      [Dedukti.declaration name const_type']
+      Dedukti.print out (Dedukti.declaration name const_type')
   | Def(constr_substituted) ->
       let constr' = Terms.translate_constr env (Declarations.force constr_substituted) in
-      [Dedukti.definition false name const_type' constr']
+      Dedukti.print out (Dedukti.definition false name const_type' constr')
   | OpaqueDef(lazy_constr) ->
       let constr' = Terms.translate_constr env (Declarations.force_opaque lazy_constr) in
-      [Dedukti.definition true name const_type' constr']
+      Dedukti.print out (Dedukti.definition true name const_type' constr')
 
-let rec translate_module_body env module_body =
+let rec translate_module_body out env module_body =
   match module_body.mod_expr with
-  | Some(struct_expr_body) -> translate_struct_expr_body env struct_expr_body
+  | Some(struct_expr_body) -> translate_struct_expr_body out env struct_expr_body
   | None -> failwith "Empty module body"
 
-and translate_struct_expr_body env struct_expr_body =
+and translate_struct_expr_body out env struct_expr_body =
   match struct_expr_body with
-  | SEBstruct(structure_body) -> translate_structure_body env structure_body
-  | _ -> []
+  | SEBstruct(structure_body) -> translate_structure_body out env structure_body
+  | _ -> ()
 
-and translate_structure_body env structure_body =
-  List.concat (List.map (translate_structure_field_body env) structure_body)
+and translate_structure_body out env structure_body =
+  List.iter (translate_structure_field_body out env) structure_body
 
-and translate_structure_field_body env (label, structure_field_body) =
+and translate_structure_field_body out env (label, structure_field_body) =
   match structure_field_body with
-  | SFBconst(constant_body) -> translate_constant_body env label constant_body
-  | _ -> []
+  | SFBconst(constant_body) -> translate_constant_body out env label constant_body
+  | _ -> ()
 
