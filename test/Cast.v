127cc6743515f23a6161743c78edb6d6ab92926f
Ali Assaf <ali.assaf@inria.fr>
2013-07-19 13:37:14 +0200

    Improve the cast test file.

--- test/Cast.v
+++ test/Cast.v
@@ -2,3 +2,13 @@ Parameter x : Prop.
 
 Definition y : Type := (x : Type).
 
+Definition f : Prop -> Type := ((fun x : Prop => x) : Prop -> Type).
+
+Definition g : Prop -> Type := fun x : Prop => (x : Type).
+
+Parameter P : (Prop -> Type) -> Type.
+
+Parameter h : forall f : (Prop -> Type), P f.
+
+Definition z : P f := h g.
+
