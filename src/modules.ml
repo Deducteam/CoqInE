f7f5ce83f1bfd40dd118d25f481cd54a741ebe53
Ali Assaf <ali.assaf@inria.fr>
2013-07-20 18:26:33 +0200

    Change the name of the variables to better represent their type.

--- src/modules.ml
+++ src/modules.ml
@@ -30,21 +30,21 @@ let get_inductive_arity_sort ind_arity =
 
 (** Translate the inductive body [ind] belonging to the mutual inductive
     body [mind]. *)
-let translate_one_inductive_body out module_path env ind label mind =
+let translate_one_inductive_body out module_path env ind_body label mind_body =
   (* Translate the inductive type. *)
-  let name = ind.mind_typename in
-  let sort = get_inductive_arity_sort ind.mind_arity in
-  let arity = Term.it_mkProd_or_LetIn (Term.mkSort sort) ind.mind_arity_ctxt in
+  let name = ind_body.mind_typename in
+  let sort = get_inductive_arity_sort ind_body.mind_arity in
+  let arity = Term.it_mkProd_or_LetIn (Term.mkSort sort) ind_body.mind_arity_ctxt in
   let name' = Name.translate_identifier name in
   let arity' = Terms.translate_types out env arity in
   Dedukti.print out (Dedukti.declaration name' arity');
   (* Translate the constructors. *)
   (* Substitute the inductive type names as specified in the Coq code. *)
-  let mind_name = Names.make_mind module_path Names.empty_dirpath label in
-  let mind_names = Array.init mind.mind_ntypes (fun i -> Term.mkInd(mind_name, i)) in
-  for j = 0 to Array.length ind.mind_consnames - 1 do
-    let cname = ind.mind_consnames.(j) in
-    let ctype = Term.substl (Array.to_list mind_names) ind.mind_user_lc.(j) in
+  let mind = Names.make_mind module_path Names.empty_dirpath label in
+  let inds = Array.init mind_body.mind_ntypes (fun i -> Term.mkInd(mind, i)) in
+  for j = 0 to Array.length ind_body.mind_consnames - 1 do
+    let cname = ind_body.mind_consnames.(j) in
+    let ctype = Term.substl (Array.to_list inds) ind_body.mind_user_lc.(j) in
     let cname' = Name.translate_identifier cname in
     let ctype' = Terms.translate_types out env ctype in
     Dedukti.print out (Dedukti.declaration cname' ctype');
@@ -53,9 +53,9 @@ let translate_one_inductive_body out module_path env ind label mind =
   (* Generate the fix function. *)
 
 (** Translate the body of mutual inductive definitions [mind]. *)
-let translate_mutual_inductive_body out module_path env label mind =
-  for i = 0 to mind.mind_ntypes - 1 do
-    translate_one_inductive_body out module_path env mind.mind_packets.(i) label mind
+let translate_mutual_inductive_body out module_path env label mind_body =
+  for i = 0 to mind_body.mind_ntypes - 1 do
+    translate_one_inductive_body out module_path env mind_body.mind_packets.(i) label mind_body
   done
 
 let rec translate_module_body out module_path env mods =
@@ -68,12 +68,12 @@ and translate_struct_expr_body out module_path env struct_expr =
   | SEBstruct(structs) -> translate_structure_body out module_path env structs
   | _ -> ()
 
-and translate_structure_body out module_path env structs =
-  List.iter (translate_structure_field_body out module_path env) structs
+and translate_structure_body out module_path env structure_body =
+  List.iter (translate_structure_field_body out module_path env) structure_body
 
-and translate_structure_field_body out module_path env (label, struct_field) =
-  match struct_field with
-  | SFBconst(const) -> translate_constant_body out module_path env label const
-  | SFBmind(mind) -> translate_mutual_inductive_body out module_path env label mind
+and translate_structure_field_body out module_path env (label, struct_field_body) =
+  match struct_field_body with
+  | SFBconst(const_body) -> translate_constant_body out module_path env label const_body
+  | SFBmind(mind_body) -> translate_mutual_inductive_body out module_path env label mind_body
   | _ -> ()
 
