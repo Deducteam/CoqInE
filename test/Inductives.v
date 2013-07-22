7bafee2eb2d74df4581f5dcfe51e09ae3a0725a4
Ali Assaf <ali.assaf@inria.fr>
2013-07-22 21:30:56 +0200

    Test the inductive parameters.

--- test/Inductives.v
+++ test/Inductives.v
@@ -1,8 +1,8 @@
-Inductive eq : forall (A : Type) (x : A), A -> Type :=
-| eq_refl : forall (A : Type) (x : A), eq A x x.
+Inductive eq (A : Type) (x : A) : A -> Type :=
+| eq_refl : eq A x x.
 
 Definition f (A : Type) (x : A) (y : A) (p : eq A x y) : A :=
 match p with
-| eq_refl B z => z
+| eq_refl => x
 end.
 
