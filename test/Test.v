3a10c8846aef60af63b1cddc6919258c37d6884c
Ali Assaf <ali.assaf@inria.fr>
2013-07-28 23:52:54 +0200

    Add some files from the standard library in the test suite.

--- test/Test.v
+++ test/Test.v
@@ -4,22 +4,61 @@ Require Dedukti.
 
 Dedukti Set Destination "test".
 
-Require Identity.
-Require ImportA.
-Require ImportB.
-Require ImportC.
-Require Cast.
-Require Let.
-Require Inductives.
-Require Fixpoints.
-Require Sections.
-Require NestedModules.
-Require NestedLibraries.
-Require Reflexivity.
+Require
+        Identity
+        ImportA
+        ImportB
+        ImportC
+        Cast
+        Let
+        Inductives
+        Fixpoints
+        Sections
+        NestedModules
+        NestedLibraries
+        Reflexivity
+        .
 
-Require Init.
-Require Logic.
-(*Require Arith.*)
+Require
+        Coq.Init.Datatypes
+        Coq.Init.Logic_Type
+        Coq.Init.Logic
+        Coq.Init.Notations
+        Coq.Init.Peano
+        Coq.Init.Prelude
+        Coq.Init.Specif
+        Coq.Init.Tactics
+        Coq.Init.Wf
+        
+(*        Coq.Logic.Berardi*)
+(*        Coq.Logic.ChoiceFacts*)
+(*        Coq.Logic.ClassicalChoice*)
+(*        Coq.Logic.ClassicalDescription*)
+(*        Coq.Logic.ClassicalEpsilon*)
+(*        Coq.Logic.ClassicalFacts*)
+(*        Coq.Logic.Classical_Pred_Set*)
+(*        Coq.Logic.Classical_Pred_Type*)
+(*        Coq.Logic.Classical_Prop*)
+(*        Coq.Logic.Classical_Type*)
+(*        Coq.Logic.ClassicalUniqueChoice*)
+(*        Coq.Logic.Classical*)
+(*        Coq.Logic.ConstructiveEpsilon*)
+(*        Coq.Logic.Decidable*)
+(*        Coq.Logic.Description*)
+(*        Coq.Logic.Diaconescu*)
+(*        Coq.Logic.Epsilon*)
+(*        Coq.Logic.Eqdep_dec*)
+(*        Coq.Logic.EqdepFacts*)
+(*        Coq.Logic.Eqdep*)
+(*        Coq.Logic.FunctionalExtensionality*)
+(*        Coq.Logic.Hurkens*)
+(*        Coq.Logic.IndefiniteDescription*)
+(*        Coq.Logic.JMeq*)
+(*        Coq.Logic.ProofIrrelevanceFacts*)
+(*        Coq.Logic.ProofIrrelevance*)
+(*        Coq.Logic.RelationalChoice*)
+(*        Coq.Logic.SetIsType*)
+          .
 
 Dedukti All Export.
 
