faafd6d4722b74bb14e49af5e1c44243500e00b9
aliassaf <ali.assaf@inria.fr>
2014-01-28 18:27:39 +0100

    Add a separate makefile for testing. Update to the makefiles to the new dedukti.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 COQ_MAKEFILE = coq_makefile
 COQTOP = coqtop
+DKCHECK = dkcheck
 
 MAKEFILE_PLUGIN = Makefile.plugin
-MAKEFILE_TEST = Makefile.test
 
 .PHONY: plugin install test clean
 
@@ -12,13 +12,14 @@ plugin:
 install:
 	$(COQ_MAKEFILE) -f $(MAKEFILE_PLUGIN) | make -f - install
 
-test: plugin
-	$(COQ_MAKEFILE) -f $(MAKEFILE_TEST) | make -f -
-	$(COQTOP) -batch -nois -R src Dedukti -I test -l test/Test
-	camelide -v 1 dedukti/Coq.dk test/*.dk
+test: plugin Coq.dko
+	make -C test
+
+Coq.dko: dedukti/Coq.dk
+	$(DKCHECK) -e -unsafe dedukti/Coq.dk
 
 clean:
 	$(COQ_MAKEFILE) -f $(MAKEFILE_PLUGIN) | make -f - clean
-	$(COQ_MAKEFILE) -f $(MAKEFILE_TEST) | make -f - clean
-	rm -rf test/*.dk
+	make -C test clean
+	rm -rf Coq.dko
 
--- test/Test.v
+++ test/Test.v
@@ -2,63 +2,62 @@
 
 Require Dedukti.
 
-Dedukti Set Destination "test".
+Dedukti Set Destination "".
 
 Require
-        Identity
-        ImportA
-        ImportB
-        ImportC
-        Cast
-        Let
-        Inductives
-        Fixpoints
-        Sections
-        NestedModules
-        NestedLibraries
-        Reflexivity
-        .
 
-Require
-        Coq.Init.Datatypes
-        Coq.Init.Logic_Type
-        Coq.Init.Logic
-        Coq.Init.Notations
-        Coq.Init.Peano
-        Coq.Init.Prelude
-        Coq.Init.Specif
-        Coq.Init.Tactics
-        Coq.Init.Wf
-        
-(*        Coq.Logic.Berardi*)
-(*        Coq.Logic.ChoiceFacts*)
-(*        Coq.Logic.ClassicalChoice*)
-(*        Coq.Logic.ClassicalDescription*)
-(*        Coq.Logic.ClassicalEpsilon*)
-(*        Coq.Logic.ClassicalFacts*)
-(*        Coq.Logic.Classical_Pred_Set*)
-(*        Coq.Logic.Classical_Pred_Type*)
-(*        Coq.Logic.Classical_Prop*)
-(*        Coq.Logic.Classical_Type*)
-(*        Coq.Logic.ClassicalUniqueChoice*)
-(*        Coq.Logic.Classical*)
-(*        Coq.Logic.ConstructiveEpsilon*)
-(*        Coq.Logic.Decidable*)
-(*        Coq.Logic.Description*)
-(*        Coq.Logic.Diaconescu*)
-(*        Coq.Logic.Epsilon*)
-(*        Coq.Logic.Eqdep_dec*)
-(*        Coq.Logic.EqdepFacts*)
-(*        Coq.Logic.Eqdep*)
-(*        Coq.Logic.FunctionalExtensionality*)
-(*        Coq.Logic.Hurkens*)
-(*        Coq.Logic.IndefiniteDescription*)
-(*        Coq.Logic.JMeq*)
-(*        Coq.Logic.ProofIrrelevanceFacts*)
-(*        Coq.Logic.ProofIrrelevance*)
-(*        Coq.Logic.RelationalChoice*)
-(*        Coq.Logic.SetIsType*)
-          .
+  Identity
+  ImportA
+  ImportB
+  ImportC
+  Cast
+  Let
+  Inductives
+  Fixpoints
+  Sections
+  NestedModules
+  NestedLibraries
+  Reflexivity
+
+  Coq.Init.Datatypes
+  Coq.Init.Logic_Type
+  Coq.Init.Logic
+  Coq.Init.Notations
+  Coq.Init.Peano
+  Coq.Init.Prelude
+  Coq.Init.Specif
+  Coq.Init.Tactics
+  Coq.Init.Wf        
+
+  Coq.Logic.Berardi
+(*  Coq.Logic.ChoiceFacts*)
+(*  Coq.Logic.ClassicalChoice*)
+(*  Coq.Logic.ClassicalDescription*)
+(*  Coq.Logic.ClassicalEpsilon*)
+(*  Coq.Logic.ClassicalFacts*)
+(*  Coq.Logic.Classical_Pred_Set*)
+(*  Coq.Logic.Classical_Pred_Type*)
+(*  Coq.Logic.Classical_Prop*)
+(*  Coq.Logic.Classical_Type*)
+(*  Coq.Logic.ClassicalUniqueChoice*)
+(*  Coq.Logic.Classical*)
+(*  Coq.Logic.ConstructiveEpsilon*)
+(*  Coq.Logic.Decidable*)
+(*  Coq.Logic.Description*)
+(*  Coq.Logic.Diaconescu*)
+(*  Coq.Logic.Epsilon*)
+(*  Coq.Logic.Eqdep_dec*)
+(*  Coq.Logic.EqdepFacts*)
+(*  Coq.Logic.Eqdep*)
+(*  Coq.Logic.FunctionalExtensionality*)
+(*  Coq.Logic.Hurkens*)
+(*  Coq.Logic.IndefiniteDescription*)
+(*  Coq.Logic.JMeq*)
+(*  Coq.Logic.ProofIrrelevanceFacts*)
+(*  Coq.Logic.ProofIrrelevance*)
+(*  Coq.Logic.RelationalChoice*)
+(*  Coq.Logic.SetIsType*)
+.
 
 Dedukti All Export.
 
