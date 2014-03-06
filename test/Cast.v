83734eeeb0702e0b2dc171c5ffefde7434454578
aliassaf <ali.assaf@inria.fr>
2014-03-06 14:23:02 +0100

    Add additional tests for testing uniqueness of term representation under casts.

--- test/Cast.v
+++ test/Cast.v
@@ -12,3 +12,29 @@ Parameter h : forall f : (Prop -> Type), P f.
 
 Definition z : P f := h g.
 
+Definition Type3 : Type := Type.
+Definition Type2 : Type3 := Type.
+Definition Type1 : Type2 := Type.
+Definition Type0 : Type1 := Type.
+
+Definition type_canonicity
+  (A : Type0)
+  (x : forall (A : Type3), A)
+  (y : A -> A)
+:=
+  y (x A).
+
+(*Definition foo (A : Type) (B : Prop) : Prop := (fun (B : Type0) => forall (x : A), B) B.*)
+
+(* Add a parameter to freeze reduction and force conversion on the term representation. *)
+
+Parameter F : Type0 -> Type0.
+
+Definition prop_canonicity
+  (A : Type0)
+  (B : Prop)
+  (x : F ((fun (B : Type0) => forall (x : A), B) B))
+:
+  F ((fun C : Type0 => C) (forall (x : A), B))
+:=
+  x.
