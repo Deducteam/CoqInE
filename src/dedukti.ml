86ac6fa8c77c4ff0539ffda5ceba1b741050c345
Ali Assaf <ali.assaf@inria.fr>
2014-03-17 15:51:52 +0100

    Print using pretty-printing boxes and hints to get a nice readable output.

--- src/dedukti.ml
+++ src/dedukti.ml
@@ -64,22 +64,28 @@ let print_var out x =
   | _ -> Format.fprintf out "%s" x
 
 let rec print_term out term =
-  match term with
-  | Pie(("", a), b) ->
-    Format.fprintf out "%a -> %a" print_app a print_term b
-  | Pie((x, a), b) ->
-    Format.fprintf out "%a : %a -> %a" print_var x print_app a print_term b
-  | Lam((x, a), b) ->
-    Format.fprintf out "%a : %a => %a" print_var x print_app a print_term b
-  | _ ->
-    Format.fprintf out "%a" print_app term
+  let rec print_term out term =
+    match term with
+    | Pie(("", a), b) ->
+      Format.fprintf out "%a ->@ %a" print_app a print_term b
+    | Pie((x, a), b) ->
+      Format.fprintf out "%a ->@ %a" print_binding (x, a) print_term b
+    | Lam((x, a), b) ->
+      Format.fprintf out "%a =>@ %a" print_binding (x, a) print_term b
+    | _ ->
+      Format.fprintf out "%a" print_app term
+  in
+  Format.fprintf out "@[<hv0>%a@]" print_term term
 
 and print_app out term =
-  match term with
-  | App(a, b) ->
-    Format.fprintf out "%a %a" print_app a print_atomic b
-  | _ ->
-    Format.fprintf out "%a" print_atomic term
+  let rec print_app out term =
+    match term with
+    | App(a, b) ->
+      Format.fprintf out "%a@ %a" print_app a print_atomic b
+    | _ ->
+      Format.fprintf out "%a" print_atomic term
+  in
+  Format.fprintf out "@[<2>%a@]" print_app term
 
 and print_atomic out term =
   match term with
@@ -94,32 +100,35 @@ and print_atomic out term =
   | _ ->
     Format.fprintf out "(%a)" print_term term
 
-let print_binding out (x, a) =
-  Format.fprintf out "%a : %a" print_var x print_term a
+and print_binding out (x, a) =
+  Format.fprintf out "@[<2>%a :@ %a@]" print_var x print_app a
 
-let rec print_context out context =
-  match context with
-  | [] -> ()
-  | [xa] -> Format.fprintf out "%a" print_binding xa
-  | xa :: context -> Format.fprintf out "%a, %a" print_binding xa print_context context
-
-let print_rule out (context, left, right) =
-  Format.fprintf out "[%a]\n  %a -->\n  %a\n" print_context context print_term left print_term right
+let print_context out context =
+  let rec print_context out context =
+    match context with
+    | [] -> ()
+    | [xa] -> Format.fprintf out "%a" print_binding xa
+    | xa :: context -> Format.fprintf out "%a,@ %a" print_binding xa print_context context
+  in
+  Format.fprintf out "@[<v>%a@]" print_context context
 
 let print out instruction =
-  match instruction with
+  begin match instruction with
   | Comment(c) ->
-      Format.fprintf out "(; %s ;)\n\n" c
+      Format.fprintf out "(; %s ;)" c
   | Command(cmd, args) ->
       let print_args out = List.iter (Format.fprintf out " %s") in
-      Format.fprintf out "#%s%a\n\n" cmd print_args args
+      Format.fprintf out "#%s%a" cmd print_args args
   | Declaration(x, a) ->
-      Format.fprintf out "%a : %a.\n\n" print_var x print_term a
+      Format.fprintf out "@[<v2>%a :@ @ %a.@]" print_var x print_term a
   | Definition(opaque, x, a, t) ->
       let print_opaque out =
         if opaque then Format.fprintf out "{%a}" print_var
         else Format.fprintf out "%a" print_var in
-      Format.fprintf out "%a : %a :=\n  %a.\n\n" print_opaque x print_term a print_term t
+      Format.fprintf out "@[<v2>%a :@ @ %a :=@ @ %a.@]" print_opaque x print_term a print_term t
   | Rewrite(context, left, right) ->
-      Format.fprintf out "%a.\n\n" print_rule (context, left, right)
+      Format.fprintf out "@[<v2>[ %a]@ @ %a -->@ @ %a.@]" print_context context print_term left print_term right
+  end;
+  Format.pp_print_newline out ();
+  Format.pp_print_newline out ()
 
